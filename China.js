f3c2308566559192416a03b01e18c37b5e6d8d3b
KenBoi26 <kennethphilip23@lpu.in>
2024-04-14 19:39:43 +0530

    china also done

---
+++ China.js
@@ -0,0 +1,62 @@
+// Loading of the Google Charts API
+google.charts.load('current', {'packages':['corechart']});
+
+// Callback on when Google Charts is open
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+  // Creating the data table
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Year');
+  data.addColumn('number', 'Population (in millions)');
+  data.addRows([
+    ["2014", 1385],
+    ["2015", 1393],
+    ["2016", 1401],
+    ["2017", 1410],
+    ["2018", 1417],
+    ["2019", 1421],
+    ["2020", 1424],
+    ["2021", 1425.89],
+    ["2022", 1425.88],
+    ["2023", 1425.67],
+    ["2024", 1425.17]
+  ]);
+
+  // Setting the chart options
+  var options = {
+    hAxis: {
+      title: 'Year'
+    },
+    vAxis: {
+      title: 'Population (in millions)'
+    },
+    pointSize: 10,
+    pointShape: 'circle',
+    animation: {
+      duration: 1000,
+      easing: 'out',
+      startup: true
+    },
+    colors: ['#008000']
+  };
+
+  var chart = new google.visualization.LineChart(document.getElementById('lineChart'));
+  google.visualization.events.addListener(chart, 'select', selectHandler);
+  chart.draw(data, options);
+
+  // Scrolling to its respective content
+  function selectHandler() {
+    var selection = chart.getSelection();
+    if (selection.length > 0) {
+      var row = selection[0].row;
+      var id = 'info' + (row + 1);
+      var section = document.getElementById(id);
+      section.classList.add('highlight');
+      setTimeout(function() {
+        section.classList.remove('highlight');
+      }, 1010);
+      section.scrollIntoView({behavior: 'smooth'});
+    }
+  }
+}
